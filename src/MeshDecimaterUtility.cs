badb4ac043b27ff59392feb46431fb616658faf1
Refia <112306763+refiaa@users.noreply.github.com>
2024-06-12 03:54:39 +0900

    240612.0353
    ・他のcomponentsも維持できるように修正(仮)

--- src/MeshDecimaterUtility.cs
+++ src/MeshDecimaterUtility.cs
@@ -17,32 +17,37 @@ public static class MeshDecimaterUtility
 
         Mesh simplifiedMesh = meshSimplifier.ToMesh();
 
-        mesh.Clear();
-        mesh.vertices = simplifiedMesh.vertices;
-        mesh.triangles = simplifiedMesh.triangles;
-        mesh.normals = simplifiedMesh.normals;
-        mesh.uv = simplifiedMesh.uv;
-        mesh.bindposes = simplifiedMesh.bindposes;
-        mesh.boneWeights = simplifiedMesh.boneWeights;
+        CopyMeshData(simplifiedMesh, mesh);
+    }
 
-        for (int i = 0; i < simplifiedMesh.blendShapeCount; i++)
+    private static void CopyMeshData(Mesh sourceMesh, Mesh targetMesh)
+    {
+        targetMesh.Clear();
+        targetMesh.vertices = sourceMesh.vertices;
+        targetMesh.triangles = sourceMesh.triangles;
+        targetMesh.normals = sourceMesh.normals;
+        targetMesh.uv = sourceMesh.uv;
+        targetMesh.bindposes = sourceMesh.bindposes;
+        targetMesh.boneWeights = sourceMesh.boneWeights; 
+
+        for (int i = 0; i < sourceMesh.blendShapeCount; i++)
         {
-            string shapeName = simplifiedMesh.GetBlendShapeName(i);
-            int frameCount = simplifiedMesh.GetBlendShapeFrameCount(i);
+            string shapeName = sourceMesh.GetBlendShapeName(i);
+            int frameCount = sourceMesh.GetBlendShapeFrameCount(i);
 
             for (int j = 0; j < frameCount; j++)
             {
-                float frameWeight = simplifiedMesh.GetBlendShapeFrameWeight(i, j);
-                Vector3[] deltaVertices = new Vector3[simplifiedMesh.vertexCount];
-                Vector3[] deltaNormals = new Vector3[simplifiedMesh.vertexCount];
-                Vector3[] deltaTangents = new Vector3[simplifiedMesh.vertexCount];
+                float frameWeight = sourceMesh.GetBlendShapeFrameWeight(i, j);
+                Vector3[] deltaVertices = new Vector3[sourceMesh.vertexCount];
+                Vector3[] deltaNormals = new Vector3[sourceMesh.vertexCount];
+                Vector3[] deltaTangents = new Vector3[sourceMesh.vertexCount];
 
-                simplifiedMesh.GetBlendShapeFrameVertices(i, j, deltaVertices, deltaNormals, deltaTangents);
-                mesh.AddBlendShapeFrame(shapeName, frameWeight, deltaVertices, deltaNormals, deltaTangents);
+                sourceMesh.GetBlendShapeFrameVertices(i, j, deltaVertices, deltaNormals, deltaTangents);
+                targetMesh.AddBlendShapeFrame(shapeName, frameWeight, deltaVertices, deltaNormals, deltaTangents);
             }
         }
 
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
+        targetMesh.RecalculateBounds();
+        targetMesh.RecalculateNormals();
     }
 }
