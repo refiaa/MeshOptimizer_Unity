ba0341153d113b6ee16af44eb99ce9794b1675a8
Refia <112306763+refiaa@users.noreply.github.com>
2024-06-11 20:33:23 +0900

    Update MeshUtils.cs

--- src/MeshUtils.cs
+++ src/MeshUtils.cs
@@ -5,11 +5,11 @@ public static class MeshUtils
     public static float CalculateMeshSize(Mesh mesh)
     {
         float size = 0;
-        size += mesh.vertexCount * sizeof(float) * 3; // vertices
-        size += mesh.normals.Length * sizeof(float) * 3; // normals
-        size += mesh.uv.Length * sizeof(float) * 2; // UVs
-        size += mesh.triangles.Length * sizeof(int); // triangles
+        size += mesh.vertexCount * sizeof(float) * 3;
+        size += mesh.normals.Length * sizeof(float) * 3; 
+        size += mesh.uv.Length * sizeof(float) * 2;
+        size += mesh.triangles.Length * sizeof(int);
 
-        return size / 1024; // convert to KB
+        return size / 1024;
     }
 }
