8bc53f423f7bbdcb3d82bfccc686ebe7b0243570
Refia <112306763+refiaa@users.noreply.github.com>
2024-06-12 11:59:01 +0900

    240612.1157
    roll back to 240611.2335 with better way to fix BlendShapes problem

--- src/DecimaterMain.cs
+++ src/DecimaterMain.cs
@@ -106,7 +106,7 @@ public class DecimaterMain : EditorWindow
     private void ApplyDecimation()
     {
         EnableReadWrite(decimatedMesh);
-        MeshDecimaterUtility.DecimateMesh(decimatedMesh, decimateLevel);
+        MeshDecimaterUtility.DecimateMesh(originalMesh, decimatedMesh, decimateLevel);
 
         if (selectedGameObject.GetComponent<MeshFilter>() != null)
         {
@@ -118,7 +118,6 @@ public class DecimaterMain : EditorWindow
             skinnedMeshRenderer.sharedMesh = decimatedMesh;
             skinnedMeshRenderer.sharedMesh.bindposes = originalMesh.bindposes;
             skinnedMeshRenderer.sharedMesh.boneWeights = originalMesh.boneWeights;
-            PreserveBlendShapes(originalMesh, skinnedMeshRenderer.sharedMesh);
         }
 
         meshPreviewer.UpdatePreviewMesh(selectedGameObject);
@@ -168,27 +167,6 @@ public class DecimaterMain : EditorWindow
         {
             modelImporter.isReadable = true;
             AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
-        }
-    }
-
-    private void PreserveBlendShapes(Mesh originalMesh, Mesh decimatedMesh)
-    {
-        int blendShapeCount = originalMesh.blendShapeCount;
-        for (int i = 0; i < blendShapeCount; i++)
-        {
-            string shapeName = originalMesh.GetBlendShapeName(i);
-            int frameCount = originalMesh.GetBlendShapeFrameCount(i);
-
-            for (int j = 0; j < frameCount; j++)
-            {
-                float frameWeight = originalMesh.GetBlendShapeFrameWeight(i, j);
-                Vector3[] deltaVertices = new Vector3[originalMesh.vertexCount];
-                Vector3[] deltaNormals = new Vector3[originalMesh.vertexCount];
-                Vector3[] deltaTangents = new Vector3[originalMesh.vertexCount];
-
-                originalMesh.GetBlendShapeFrameVertices(i, j, deltaVertices, deltaNormals, deltaTangents);
-                decimatedMesh.AddBlendShapeFrame(shapeName, frameWeight, deltaVertices, deltaNormals, deltaTangents);
-            }
         }
     }
 }
--- src/MeshDecimaterUtility.cs
+++ src/MeshDecimaterUtility.cs
@@ -7,47 +7,84 @@ using UnityMeshSimplifier from github.com/Whinarn/UnityMeshSimplifier
 
 public static class MeshDecimaterUtility
 {
-    public static void DecimateMesh(Mesh mesh, float decimateLevel)
+    public static void DecimateMesh(Mesh originalMesh, Mesh decimatedMesh, float decimateLevel)
     {
         MeshSimplifier meshSimplifier = new MeshSimplifier();
 
-        meshSimplifier.Initialize(mesh);
+        meshSimplifier.Initialize(originalMesh);
 
         meshSimplifier.SimplifyMesh(decimateLevel);
 
         Mesh simplifiedMesh = meshSimplifier.ToMesh();
 
-        CopyMeshData(simplifiedMesh, mesh);
+        decimatedMesh.Clear();
+        decimatedMesh.vertices = simplifiedMesh.vertices;
+        decimatedMesh.triangles = simplifiedMesh.triangles;
+        decimatedMesh.normals = simplifiedMesh.normals;
+        decimatedMesh.uv = simplifiedMesh.uv;
+        decimatedMesh.bindposes = simplifiedMesh.bindposes;
+        decimatedMesh.boneWeights = simplifiedMesh.boneWeights;
+
+        CopyBlendShapes(originalMesh, simplifiedMesh, decimatedMesh);
+
+        decimatedMesh.RecalculateBounds();
+        decimatedMesh.RecalculateNormals();
     }
 
-    private static void CopyMeshData(Mesh sourceMesh, Mesh targetMesh)
+    private static void CopyBlendShapes(Mesh originalMesh, Mesh simplifiedMesh, Mesh targetMesh)
     {
-        targetMesh.Clear();
-        targetMesh.vertices = sourceMesh.vertices;
-        targetMesh.triangles = sourceMesh.triangles;
-        targetMesh.normals = sourceMesh.normals;
-        targetMesh.uv = sourceMesh.uv;
-        targetMesh.bindposes = sourceMesh.bindposes;
-        targetMesh.boneWeights = sourceMesh.boneWeights; 
-
-        for (int i = 0; i < sourceMesh.blendShapeCount; i++)
+        for (int i = 0; i < originalMesh.blendShapeCount; i++)
         {
-            string shapeName = sourceMesh.GetBlendShapeName(i);
-            int frameCount = sourceMesh.GetBlendShapeFrameCount(i);
+            string shapeName = originalMesh.GetBlendShapeName(i);
+            int frameCount = originalMesh.GetBlendShapeFrameCount(i);
 
-            for (int j = 0; j < frameCount; j++)
+            for (int frameIndex = 0; frameIndex < frameCount; frameIndex++)
             {
-                float frameWeight = sourceMesh.GetBlendShapeFrameWeight(i, j);
-                Vector3[] deltaVertices = new Vector3[sourceMesh.vertexCount];
-                Vector3[] deltaNormals = new Vector3[sourceMesh.vertexCount];
-                Vector3[] deltaTangents = new Vector3[sourceMesh.vertexCount];
+                float frameWeight = originalMesh.GetBlendShapeFrameWeight(i, frameIndex);
+                Vector3[] deltaVertices = new Vector3[simplifiedMesh.vertexCount];
+                Vector3[] deltaNormals = new Vector3[simplifiedMesh.vertexCount];
+                Vector3[] deltaTangents = new Vector3[simplifiedMesh.vertexCount];
 
-                sourceMesh.GetBlendShapeFrameVertices(i, j, deltaVertices, deltaNormals, deltaTangents);
+                SimplifyBlendShapeFrame(originalMesh, simplifiedMesh, i, frameIndex, deltaVertices, deltaNormals, deltaTangents);
                 targetMesh.AddBlendShapeFrame(shapeName, frameWeight, deltaVertices, deltaNormals, deltaTangents);
             }
         }
+    }
 
-        targetMesh.RecalculateBounds();
-        targetMesh.RecalculateNormals();
+    private static void SimplifyBlendShapeFrame(Mesh originalMesh, Mesh simplifiedMesh, int shapeIndex, int frameIndex, Vector3[] deltaVertices, Vector3[] deltaNormals, Vector3[] deltaTangents)
+    {
+        Vector3[] originalVertices = originalMesh.vertices;
+        Vector3[] simplifiedVertices = simplifiedMesh.vertices;
+        Vector3[] originalDeltaVertices = new Vector3[originalMesh.vertexCount];
+        Vector3[] originalDeltaNormals = new Vector3[originalMesh.vertexCount];
+        Vector3[] originalDeltaTangents = new Vector3[originalMesh.vertexCount];
+
+        originalMesh.GetBlendShapeFrameVertices(shapeIndex, frameIndex, originalDeltaVertices, originalDeltaNormals, originalDeltaTangents);
+
+        for (int i = 0; i < simplifiedVertices.Length; i++)
+        {
+            int closestVertexIndex = FindClosestVertex(originalVertices, simplifiedVertices[i]);
+            deltaVertices[i] = originalDeltaVertices[closestVertexIndex];
+            deltaNormals[i] = originalDeltaNormals[closestVertexIndex];
+            deltaTangents[i] = originalDeltaTangents[closestVertexIndex];
+        }
+    }
+
+    private static int FindClosestVertex(Vector3[] vertices, Vector3 targetVertex)
+    {
+        int closestIndex = 0;
+        float closestDistance = Vector3.Distance(vertices[0], targetVertex);
+
+        for (int i = 1; i < vertices.Length; i++)
+        {
+            float distance = Vector3.Distance(vertices[i], targetVertex);
+            if (distance < closestDistance)
+            {
+                closestIndex = i;
+                closestDistance = distance;
+            }
+        }
+
+        return closestIndex;
     }
 }
